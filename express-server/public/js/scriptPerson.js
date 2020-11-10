fb516fc287ab6589f92f9320e18e04d94866d0b2
meerabal <meera.balsara@gmail.com>
2020-11-10 16:03:49 -0500

    edited scriptPerson.js

--- express-server/public/js/scriptPerson.js
+++ express-server/public/js/scriptPerson.js
@@ -13,10 +13,12 @@ function postFollowReq() {
     button.innerText = "Unfollow";
     let req = new XMLHttpRequest();
     req.open("POST", "http://localhost:3000/people/"+personID+"/follow");
+    req.send();
 }
 
 function postUnfollowReq() {
     button.innerText = "Follow";
     let req = new XMLHttpRequest();
     req.open("POST", "http://localhost:3000/people/"+personID+"/unfollow");
+    req.send();
 }
