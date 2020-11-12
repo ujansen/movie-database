20b85b79115919eaaae5d5b426706c91333fc67b
Ujan Sen <55211866+ujansen@users.noreply.github.com>
2020-11-12 16:08:43 +0530

    Update searchIMDB.js

--- express-server/public/js/searchIMDB.js
+++ express-server/public/js/searchIMDB.js
@@ -6,19 +6,13 @@ searchIMDB.addEventListener("click", searchIMDBRequest);
 
 function searchIMDBRequest() {
   console.log("clicked");
-  wait.innerText = "Please wait for a few moments until the results load...";
+  if (wait){
+    wait.innerText = "Please wait for a few moments until the results load...";
+  }
   let searchQuery = "";
   if(movieTitle.value) {
     movieTitle = movieTitle.value.toString();
     searchQuery = movieTitle.trim().split(" ").join("+");
   }
-  let req = new XMLHttpRequest();
-  req.open("GET", "http://localhost:3000/searchIMDB/"+searchQuery);
-  req.onreadystatechange = function (){
-    if(req.readyState == 4 && req.status == 200){
-      console.log(req.responseText);
-      window.location.href = req.responseText;
-   }
- };
-  req.send();
+  window.location.href = "http://localhost:3000/searchIMDB/"+searchQuery;
 }
